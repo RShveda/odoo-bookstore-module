a3efd68c2b45c309b0122c3c4fe3769984cb6076
Roman Shveda <rshveda@gmail.com>
2020-12-23 11:12:43 +0200

    Added templates and routes for index, books list, and book object pages

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -3,19 +3,21 @@ from odoo import http
 
 
 class Bookstore(http.Controller):
-    @http.route('/bookstore/bookstore/', auth='public')
+    @http.route('/bookstore/', auth='public')
     def index(self, **kw):
-        return "Hello, world"
+        return http.request.render('bookstore.index', {
+            'root': '/bookstore',
+        })
 
-#     @http.route('/bookstore/bookstore/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('bookstore.listing', {
-#             'root': '/bookstore/bookstore',
-#             'objects': http.request.env['bookstore.bookstore'].search([]),
-#         })
+    @http.route('/bookstore/books/', auth='public')
+    def list(self, **kw):
+        return http.request.render('bookstore.listing', {
+            'root': '/bookstore',
+            'objects': http.request.env['bookstore.book'].search([]),
+        })
 
-#     @http.route('/bookstore/bookstore/objects/<model("bookstore.bookstore"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('bookstore.object', {
-#             'object': obj
-#         })
+    @http.route('/bookstore/books/<model("bookstore.book"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('bookstore.object', {
+            'object': obj
+        })
